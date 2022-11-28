ddb6f12b94abf0e6dbb8827d721b6fadc7f55a23
eshika <eshika22som2003@gmail.com>
2022-11-28 20:07:38 +0530

    okay

---
+++ index.js
@@ -0,0 +1,25 @@
+//REPL
+//1: js expression
+//2: use variables
+//3: multiline code/loops
+// 4: use (_) to get the last result 
+// 5: we can use editor mode  
+
+//code modulus
+const fs=require("fs");
+const { default: nodeTest } = require("node:test");
+const { finished } = require("stream");
+// fs.writeFileSync("read.txt","welcome to coding")  //creating a new file
+// fs.writeFileSync("read.txt","eshika,welcome to coding") 
+// fs.appendFileSync("read.txt"," how are you? i am fine.thank you ;)");
+const buf_data=fs.readFileSync('read.txt');
+// console.log(buf_data);
+
+org_data=buf_data.toString();
+console.log(org_data);
+
+// node.js includes an additional data type called buffer
+// (not available in browser's js)
+// buffer is mainly used to store binary data,while reading from a file or receiving packets over the network.
+
+fs.renameSync("read.txt","readandwrite.txt"); //to rename a file
