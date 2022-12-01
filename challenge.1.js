623ffd5a0c05e2510f18d0ddab027320964b4e73
eshika <eshika22som2003@gmail.com>
2022-12-01 10:53:05 +0530

    oo

---
+++ challenge.1.js
@@ -0,0 +1,5 @@
+//CRUD:Create Read Update Delete
+const fs=require("fs");
+// fs.mkdirSync("thapa");
+// fs.writeFileSync("thapa/bio.txt","my name is eshika som.");
+// fs.appendFileSync("thapa/bio.txt"," i am an engineering student.")
