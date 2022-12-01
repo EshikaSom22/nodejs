e8afed314cde9e7e76597f425446a3a0f68998f5
eshika <eshika22som2003@gmail.com>
2022-12-01 11:06:25 +0530

    oo

--- challenge.1.js
+++ challenge.1.js
@@ -1,5 +1,10 @@
 //CRUD:Create Read Update Delete
 const fs=require("fs");
-// fs.mkdirSync("thapa");
+// fs.mkdirSync("thapa"); //creating a folder
 // fs.writeFileSync("thapa/bio.txt","my name is eshika som.");
 // fs.appendFileSync("thapa/bio.txt"," i am an engineering student.")
+// const data=fs.readFileSync("thapa/bio.txt","utf8") //reading the file without getting buffer
+// console.log(data);
+// fs.renameSync("thapa/bio.txt","mybio.txt");
+// fs.unlinkSync("mybio.txt")
+fs.rmdirSync("thapa"); //deleting the folder
--- index.js
+++ index.js
@@ -6,20 +6,22 @@
 // 5: we can use editor mode  
 
 //code modulus
-const fs=require("fs");
-const { default: nodeTest } = require("node:test");
-const { finished } = require("stream");
-// fs.writeFileSync("read.txt","welcome to coding")  //creating a new file
-// fs.writeFileSync("read.txt","eshika,welcome to coding") 
-// fs.appendFileSync("read.txt"," how are you? i am fine.thank you ;)");
-const buf_data=fs.readFileSync('read.txt');
-// console.log(buf_data);
+// const fs=require("fs");
+// const { default: nodeTest } = require("node:test");
+// const { finished } = require("stream");
+// // fs.writeFileSync("read.txt","welcome to coding")  //creating a new file
+// // fs.writeFileSync("read.txt","eshika,welcome to coding") 
+// // fs.appendFileSync("read.txt"," how are you? i am fine.thank you ;)");
+// const buf_data=fs.readFileSync('read.txt');
+// // console.log(buf_data);
 
-org_data=buf_data.toString();
-console.log(org_data);
+// org_data=buf_data.toString();
+// console.log(org_data);
 
-// node.js includes an additional data type called buffer
-// (not available in browser's js)
-// buffer is mainly used to store binary data,while reading from a file or receiving packets over the network.
+// // node.js includes an additional data type called buffer
+// // (not available in browser's js)
+// // buffer is mainly used to store binary data,while reading from a file or receiving packets over the network.
 
-fs.renameSync("read.txt","readandwrite.txt"); //to rename a file
+// // fs.renameSync("read.txt","readandwrite.txt"); //to rename a file
+
+
